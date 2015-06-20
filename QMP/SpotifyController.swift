f7f5131de57d05d5ebc7e61b3d8b678775c4b6e1
FAHAD RIAZ <fahadr@FAHADs-Mac-mini.local>
2015-06-20 13:16:06 -0400

    removing spotify integration

--- QMP/SpotifyController.swift
+++ QMP/SpotifyController.swift
@@ -1,83 +1,83 @@
+////
+////  SpotifyController.swift
+////  Kyooz
+////
+////  Created by FAHAD RIAZ on 6/6/15.
+////  Copyright (c) 2015 FAHAD RIAZ. All rights reserved.
+////
 //
-//  SpotifyController.swift
-//  Kyooz
+//import Foundation
 //
-//  Created by FAHAD RIAZ on 6/6/15.
-//  Copyright (c) 2015 FAHAD RIAZ. All rights reserved.
-//
-
-import Foundation
-
-class SpotifyController {
-    
-    static let instance:SpotifyController = SpotifyController()
-    
-    private static let clientId = "a279c29ba77242668a65420bbb025828"
-    private static let clientSecret = "337b9a0f291b4a09a49254e49c28e21c"
-    private static let callbackURL = NSURL(string: "kyooz-spotify-callback://")
-    private static let sessionUserDefaultsKey = "SPOTIFY_SESSION_KEY"
-    private static let scopes = [SPTAuthStreamingScope, SPTAuthUserLibraryReadScope, SPTAuthUserLibraryModifyScope]
-    
-    let authenticator = SPTAuth.defaultInstance()
-    
-    var sessionIsValid:Bool {
-        if let session = self.session {
-            return session.isValid()
-        }
-        return false
-    }
-    
-    var session:SPTSession! {
-        return authenticator.session
-    }
-    
-    init() {
-        authenticator.clientID = SpotifyController.clientId
-        authenticator.redirectURL = SpotifyController.callbackURL
-        authenticator.requestedScopes = SpotifyController.scopes
-        authenticator.sessionUserDefaultsKey = SpotifyController.sessionUserDefaultsKey
-    }
-    
-    func clearSession() {
-        authenticator.session = nil
-    }
-    
-    func renewSession(#completionBlock:(session:SPTSession)->()) {
-        authenticator.renewSession(session, callback: { (error:NSError!, renewedSession:SPTSession!) -> Void in
-            if error != nil {
-                Logger.debug("error occured with renewing spotify session: \(error.localizedDescription)")
-                return
-            }
-            self.authenticator.session = renewedSession
-            Logger.debug("successfully renewed session")
-            completionBlock(session: renewedSession)
-        })
-        
-    }
-    
-    func showLogInPage() {
-        
-        if let loginURL = authenticator.loginURL {
-            UIApplication.sharedApplication().openURL(loginURL)
-        } else {
-            Logger.debug("No login URL returned from spotify authenticator")
-        }
-    }
-    
-    func handleAuthenticationCallback(application:UIApplication, openURL url:NSURL, sourceApplication:String?, annotation:AnyObject?) -> Bool {
-        if(authenticator.canHandleURL(url)) {
-            authenticator.handleAuthCallbackWithTriggeredAuthURL(url, callback: { (error:NSError!, session:SPTSession!) -> Void in
-                if let err = error {
-                    Logger.debug("Spotify Authentication Error: \(error.localizedDescription)")
-                    return
-                }
-                Logger.debug("Successfully logged into Spotify!!")
-                self.authenticator.session = session
-            })
-            return true
-        }
-        
-        return false
-    }
-    
-}
+//class SpotifyController {
+//    
+//    static let instance:SpotifyController = SpotifyController()
+//    
+//    private static let clientId = "a279c29ba77242668a65420bbb025828"
+//    private static let clientSecret = "337b9a0f291b4a09a49254e49c28e21c"
+//    private static let callbackURL = NSURL(string: "kyooz-spotify-callback://")
+//    private static let sessionUserDefaultsKey = "SPOTIFY_SESSION_KEY"
+//    private static let scopes = [SPTAuthStreamingScope, SPTAuthUserLibraryReadScope, SPTAuthUserLibraryModifyScope]
+//    
+//    let authenticator = SPTAuth.defaultInstance()
+//    
+//    var sessionIsValid:Bool {
+//        if let session = self.session {
+//            return session.isValid()
+//        }
+//        return false
+//    }
+//    
+//    var session:SPTSession! {
+//        return authenticator.session
+//    }
+//    
+//    init() {
+//        authenticator.clientID = SpotifyController.clientId
+//        authenticator.redirectURL = SpotifyController.callbackURL
+//        authenticator.requestedScopes = SpotifyController.scopes
+//        authenticator.sessionUserDefaultsKey = SpotifyController.sessionUserDefaultsKey
+//    }
+//    
+//    func clearSession() {
+//        authenticator.session = nil
+//    }
+//    
+//    func renewSession(#completionBlock:(session:SPTSession)->()) {
+//        authenticator.renewSession(session, callback: { (error:NSError!, renewedSession:SPTSession!) -> Void in
+//            if error != nil {
+//                Logger.debug("error occured with renewing spotify session: \(error.localizedDescription)")
+//                return
+//            }
+//            self.authenticator.session = renewedSession
+//            Logger.debug("successfully renewed session")
+//            completionBlock(session: renewedSession)
+//        })
+//        
+//    }
+//    
+//    func showLogInPage() {
+//        
+//        if let loginURL = authenticator.loginURL {
+//            UIApplication.sharedApplication().openURL(loginURL)
+//        } else {
+//            Logger.debug("No login URL returned from spotify authenticator")
+//        }
+//    }
+//    
+//    func handleAuthenticationCallback(application:UIApplication, openURL url:NSURL, sourceApplication:String?, annotation:AnyObject?) -> Bool {
+//        if(authenticator.canHandleURL(url)) {
+//            authenticator.handleAuthCallbackWithTriggeredAuthURL(url, callback: { (error:NSError!, session:SPTSession!) -> Void in
+//                if let err = error {
+//                    Logger.debug("Spotify Authentication Error: \(error.localizedDescription)")
+//                    return
+//                }
+//                Logger.debug("Successfully logged into Spotify!!")
+//                self.authenticator.session = session
+//            })
+//            return true
+//        }
+//        
+//        return false
+//    }
+//    
+//}
--- QMP/SpotifyDAO.swift
+++ QMP/SpotifyDAO.swift
@@ -1,74 +1,74 @@
+////
+////  SpotifyDAO.swift
+////  Kyooz
+////
+////  Created by FAHAD RIAZ on 6/7/15.
+////  Copyright (c) 2015 FAHAD RIAZ. All rights reserved.
+////
 //
-//  SpotifyDAO.swift
-//  Kyooz
+//import Foundation
 //
-//  Created by FAHAD RIAZ on 6/7/15.
-//  Copyright (c) 2015 FAHAD RIAZ. All rights reserved.
+//class SpotifyDAO : MusicLibraryDAO{
+//    
+//    static let instance = SpotifyDAO()
+//    
+//    let spotifyController = SpotifyController.instance
+//    
+//    var tracks = [AudioTrack]()
+//    var artists = [String:[AudioTrack]]()
+//    
+//    func getAllTracks() {
+//        if !tracks.isEmpty { return }
 //
-
-import Foundation
-
-class SpotifyDAO : MusicLibraryDAO{
-    
-    static let instance = SpotifyDAO()
-    
-    let spotifyController = SpotifyController.instance
-    
-    var tracks = [AudioTrack]()
-    var artists = [String:[AudioTrack]]()
-    
-    func getAllTracks() {
-        if !tracks.isEmpty { return }
-
-        if spotifyController.sessionIsValid {
-            SPTYourMusic.savedTracksForUserWithAccessToken(spotifyController.session.accessToken, callback: handleTrackListPage)
-        } else {
-            spotifyController.renewSession(completionBlock: { (session) -> () in
-                SPTYourMusic.savedTracksForUserWithAccessToken(session.accessToken, callback: self.handleTrackListPage)
-            })
-        }
-    }
-    
-    
-    private func handleTrackListPage(error:NSError!, data:AnyObject!) {
-        if(error != nil) {
-            Logger.debug("error occured while getting spotify saved tracks: \(error.localizedDescription)")
-            return
-        }
-        
-        Logger.debug("\(data?.description)")
-        if let listPage = data as? SPTListPage,  let items = listPage.items as? [SPTPartialTrack] {
-            for track in items {
-                addTrack(track)
-            }
-            
-            let currentPosition = UInt(listPage.range.location + listPage.range.length)
-            let listLength = listPage.totalListLength
-            Logger.debug("Current location: \(listPage.range.location), range length: \(listPage.range.length), totalListLength:\(listLength)")
-            if(currentPosition < listLength) {
-                listPage.requestNextPageWithSession(spotifyController.session, callback: handleTrackListPage)
-            } else {
-                Logger.debug("GOT ALL THE SPOTIFY TRACKS")
-                for (artist, trackList) in artists {
-                    Logger.debug("artist available: \(artist)")
-                }
-            }
-            
-        }
-        Logger.debug("track count at end of callback block: \(self.tracks.count)")
-
-    }
-    
-    private func addTrack(track:SPTPartialTrack) {
-        tracks.append(track)
-        if var artistTracks = artists[track.albumArtist] {
-            artistTracks.append(track)
-        } else {
-            var artistTracks = [AudioTrack]()
-            artistTracks.append(track)
-            artists[track.albumArtist] = artistTracks
-        }
-    }
-    
-    
-}
+//        if spotifyController.sessionIsValid {
+//            SPTYourMusic.savedTracksForUserWithAccessToken(spotifyController.session.accessToken, callback: handleTrackListPage)
+//        } else {
+//            spotifyController.renewSession(completionBlock: { (session) -> () in
+//                SPTYourMusic.savedTracksForUserWithAccessToken(session.accessToken, callback: self.handleTrackListPage)
+//            })
+//        }
+//    }
+//    
+//    
+//    private func handleTrackListPage(error:NSError!, data:AnyObject!) {
+//        if(error != nil) {
+//            Logger.debug("error occured while getting spotify saved tracks: \(error.localizedDescription)")
+//            return
+//        }
+//        
+//        Logger.debug("\(data?.description)")
+//        if let listPage = data as? SPTListPage,  let items = listPage.items as? [SPTPartialTrack] {
+//            for track in items {
+//                addTrack(track)
+//            }
+//            
+//            let currentPosition = UInt(listPage.range.location + listPage.range.length)
+//            let listLength = listPage.totalListLength
+//            Logger.debug("Current location: \(listPage.range.location), range length: \(listPage.range.length), totalListLength:\(listLength)")
+//            if(currentPosition < listLength) {
+//                listPage.requestNextPageWithSession(spotifyController.session, callback: handleTrackListPage)
+//            } else {
+//                Logger.debug("GOT ALL THE SPOTIFY TRACKS")
+//                for (artist, trackList) in artists {
+//                    Logger.debug("artist available: \(artist)")
+//                }
+//            }
+//            
+//        }
+//        Logger.debug("track count at end of callback block: \(self.tracks.count)")
+//
+//    }
+//    
+//    private func addTrack(track:SPTPartialTrack) {
+//        tracks.append(track)
+//        if var artistTracks = artists[track.albumArtist] {
+//            artistTracks.append(track)
+//        } else {
+//            var artistTracks = [AudioTrack]()
+//            artistTracks.append(track)
+//            artists[track.albumArtist] = artistTracks
+//        }
+//    }
+//    
+//    
+//}
