ab30accd072547dd0c9e0b5e00002a51bb3c5846
fahadriaz <fahadbr@gmail.com>
2016-06-02 22:09:11 -0400

    temp

--- Kyooz/ImageUtils.swift
+++ Kyooz/ImageUtils.swift
@@ -54,50 +54,41 @@ struct ImageUtils {
             
             if albumTrackMap.count == 4 { break }
         }
+		
+		var images = albumTrackMap.map() { $0.1.artworkImage(forSize: size) }
         
         switch albumTrackMap.count {
         case 0:
             return nil
         case 1:
             return albumTrackMap.first?.1.artworkImage(forSize: size)
-        case 2, 3:
-            UIGraphicsBeginImageContextWithOptions(size, true, 0)
-
-            guard let image1 = albumTrackMap[albumTrackMap.startIndex].1.artworkImage(forSize: size) else { return nil }
-            guard let image2 = albumTrackMap[albumTrackMap.startIndex.successor()].1.artworkImage(forSize: size) else { return nil }
-            let halfWidth = size.width/2
-//            let i = CGImageCreateWithImageInRect(nil, CGRect.zero)
-//            CGImageRend
-            image1.drawInRect(CGRect(x: 0, y: 0, width: halfWidth, height: size.height))
-            image2.drawInRect(CGRect(x: halfWidth, y: 0, width: halfWidth, height: size.height))
-        case 4:
-            UIGraphicsBeginImageContextWithOptions(size, true, 0)
-            var i = 1
-            for (_, track) in albumTrackMap {
-                let halfSize = CGSize(width: size.width/2, height: size.height/2)
-                guard let image = track.artworkImage(forSize: size) else { return nil }
-                
-                let origin:CGPoint
-                switch i {
-                case 1:
-                    origin = CGPoint.zero
-                case 2:
-                    origin = CGPoint(x: halfSize.width, y: 0)
-                case 3:
-                    origin = CGPoint(x: 0, y: halfSize.height)
-                case 4:
-                    origin = CGPoint(x: halfSize.width, y: halfSize.height)
-                default:
-                    fatalError("should not be a value outside of 1...4")
-                }
-                i += 1
-                image.drawInRect(CGRect(origin: origin, size: halfSize))
-            }
+		case 2:
+			images.append(images[1])
+			images.append(images[0])
+		case 3:
+			images.append(images[0])
         default:
-            Logger.error("should not have more than 4 tracks in albumTrackMap")
-            return nil
+            break
         }
-        
+		UIGraphicsBeginImageContextWithOptions(size, true, 0)
+		for case let (i,image?) in images.enumerate() {
+			let halfSize = CGSize(width: size.width/2, height: size.height/2)
+			
+			let origin:CGPoint
+			switch i {
+			case 0:
+				origin = CGPoint.zero
+			case 1:
+				origin = CGPoint(x: halfSize.width, y: 0)
+			case 2:
+				origin = CGPoint(x: 0, y: halfSize.height)
+			case 3:
+				origin = CGPoint(x: halfSize.width, y: halfSize.height)
+			default:
+				fatalError("should not be a value outside of 0...3")
+			}
+			image.drawInRect(CGRect(origin: origin, size: halfSize))
+		}
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return image
