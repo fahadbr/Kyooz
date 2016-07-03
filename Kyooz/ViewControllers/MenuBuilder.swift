4e7e88bc496f46bbfeb4f49b011b5c533025e383
fahadriaz <fahadbr@gmail.com>
2016-07-03 15:31:04 -0400

    unstable temp commit

---
+++ Kyooz/ViewControllers/MenuBuilder.swift
@@ -0,0 +1,55 @@
+//
+//  MenuBuilder.swift
+//  Kyooz
+//
+//  Created by FAHAD RIAZ on 7/3/16.
+//  Copyright © 2016 FAHAD RIAZ. All rights reserved.
+//
+
+import UIKit
+
+final class MenuBuilder {
+	
+	lazy var presentationController = ContainerViewController.instance
+	
+	private var title: String?
+	private var details: String?
+	private var originatingCenter: CGPoint?
+	private var optionsProviders = [KyoozOptionsProvider]()
+	
+	var viewController: UIViewController {
+		return KyoozOptionsViewController(optionsProviders: optionsProviders,
+		                                  delegate: MenuOptionsDelegate(title: title,
+																		details: details,
+																		originatingCenter: originatingCenter))
+		
+		
+	}
+	
+	func with(title title: String?) -> MenuBuilder {
+		self.title = title
+		return self
+	}
+	
+	func with(details details: String?) -> MenuBuilder {
+		self.details = details
+		return self
+	}
+	
+	func with(originatingCenter originatingCenter: CGPoint?) -> MenuBuilder {
+		self.originatingCenter = originatingCenter
+		return self
+	}
+	
+	func with(optionsProviders providers: KyoozOptionsProvider...) -> MenuBuilder {
+		optionsProviders.appendContentsOf(providers)
+		return self
+	}
+	
+	func with(options options:KyoozOption...) -> MenuBuilder {
+		optionsProviders.append(BasicKyoozOptionsProvider(options: options))
+		return self
+	}
+	
+	
+}
