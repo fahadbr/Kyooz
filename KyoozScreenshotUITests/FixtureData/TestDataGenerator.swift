3f149538df0448e5090b62d39c67ce2c58891223
fahadriaz <fahadbr@gmail.com>
2016-07-16 11:08:04 -0400

    stable temp commit

---
+++ KyoozScreenshotUITests/FixtureData/TestDataGenerator.swift
@@ -0,0 +1,91 @@
+//
+//  File.swift
+//  Kyooz
+//
+//  Created by FAHAD RIAZ on 5/24/16.
+//  Copyright © 2016 FAHAD RIAZ. All rights reserved.
+//
+
+import MediaPlayer
+
+class TestDataGenerator {
+    
+    private static var randomId:UInt64 = 100100
+    
+    static func generateData() {
+		
+        let bundle = NSBundle(forClass: TestDataGenerator.self)
+        let testDataDict = NSDictionary(contentsOfURL: bundle.URLForResource("TestData", withExtension: "plist")!)!
+        
+        let artistNames = testDataDict.objectForKey("ArtistNames")! as! [String]
+        let albumNames = testDataDict.objectForKey("AlbumNames")! as! [String]
+        let trackNames = testDataDict.objectForKey("TrackNames")! as! [String]
+        
+        let trackAssetURL = bundle.URLForResource("foreword", withExtension: "mp3")!
+        
+        var images = [UIImage]()
+        for i in 1...4 {
+            images.append(UIImage(named: "Artwork\(i)", inBundle: bundle, compatibleWithTraitCollection: nil)!)
+        }
+        let artworks = images.map() { MPMediaItemArtwork(image: $0) }
+        
+
+        var tracks = [AudioTrackDTO]()
+        
+        for (k,artistName) in artistNames.enumerate() {
+            let noOfAlbums = KyoozUtils.randomNumberInRange(1...5)
+            for j in 1...noOfAlbums {
+                let albumName = albumNames[(k + j) % albumNames.count] + " \(artistName)"
+                
+                let noOfTracksInAlbum = KyoozUtils.randomNumberInRange(5...16)
+                
+                for i in 1...noOfTracksInAlbum {
+                    let dto = AudioTrackDTO()
+                    dto.trackTitle = trackNames[i - 1].capitalizedString + " \(albumName) \(artistName)"
+                    dto.albumArtist = artistName
+                    dto.artist = artistName
+                    dto.albumTitle = albumName
+                    dto.id = getRandomId()
+                    dto.albumTrackNumber = i
+                    dto.albumId = getRandomId()
+                    dto.albumArtistId = getRandomId()
+                    dto.artwork = artworks[j % artworks.count]
+                    dto.assetURL = trackAssetURL
+                    dto.releaseYear = "\(KyoozUtils.randomNumberInRange(1990...2016))"
+                    dto.playbackDuration = Double(KyoozUtils.randomNumberInRange(150...360))
+                    tracks.append(dto)
+                }
+            }
+        }
+        
+        let testSourceData = TestSourceData(tracks: tracks, grouping: LibraryGrouping.Artists)
+        
+        let artistEntities = testSourceData.entities
+        let albumEntities = TestSourceData(tracks: tracks, grouping: LibraryGrouping.Albums).entities
+        
+        let artistSE = TestSearchExecutionController(entities: artistEntities, libraryGroup: LibraryGrouping.Artists, searchKeys: ["albumArtist"])
+        let albumSE = TestSearchExecutionController(entities: albumEntities, libraryGroup: LibraryGrouping.Albums, searchKeys: ["albumTitle","albumArtist"])
+        let trackSE = TestSearchExecutionController(entities: tracks, libraryGroup: LibraryGrouping.Songs, searchKeys: ["trackTitle","albumTitle","albumArtist"])
+        let searchVC = AudioEntitySearchViewController.instance
+        searchVC.searchExecutionControllers = [artistSE, albumSE, trackSE]
+        searchVC.applyDatasourceDelegate()
+        
+        
+        
+        let rootVC = RootViewController.instance.libraryNavigationController.topViewController as! AudioEntityLibraryViewController
+        rootVC.isBaseLevel = true
+        rootVC.sourceData = testSourceData
+        rootVC.applyDataSourceAndDelegate()
+        rootVC.reloadAllData()
+	
+        
+    }
+    
+    private static func getRandomId() -> UInt64 {
+        randomId += 1
+        return randomId
+    }
+    
+    
+    
+}
---
+++ KyoozScreenshotUITests/FixtureData/TestSearchExecutionController.swift
@@ -0,0 +1,27 @@
+//
+//  TestSearchExecutionController.swift
+//  Kyooz
+//
+//  Created by FAHAD RIAZ on 5/24/16.
+//  Copyright © 2016 FAHAD RIAZ. All rights reserved.
+//
+
+import Foundation
+
+class TestSearchExecutionController : SearchExecutionController {
+    
+    let entities:[AudioEntity]
+    
+    init(entities:[AudioEntity], libraryGroup:LibraryGrouping, searchKeys:[String]) {
+        self.entities = entities
+        super.init(libraryGroup: libraryGroup, searchKeys: searchKeys)
+    }
+    
+    override func createIndexBuildingOperation() -> IndexBuildingOperation<AudioEntity>? {
+        guard let titleProp = AudioTrackDTO.titlePropertyForGrouping(libraryGroup) else { return nil }
+        return IndexBuildingOperation(parentIndexName: libraryGroup.name, valuesToIndex: entities, maxValuesAmount: 2) {[libraryGroup = self.libraryGroup] (entity) -> (String, String) in
+            return (titleProp, entity.titleForGrouping(libraryGroup)!.normalizedString)
+        }
+    }
+    
+}
