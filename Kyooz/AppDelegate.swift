dae8a97cc8ddca16f5f414f6c3dca8a1d80ada52
fahadr <fahadbr@gmail.com>
2016-11-20 23:24:34 -0500

    stable temp commit

--- Kyooz/AppDelegate.swift
+++ Kyooz/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		window = createWindow()
 		window!.rootViewController = ContainerViewController.instance
 		window!.makeKeyAndVisible()
-		
+
 		MPMediaLibrary.default().beginGeneratingLibraryChangeNotifications()
 		initMainQueueChecking()
 		ApplicationDefaults.initializeData()
